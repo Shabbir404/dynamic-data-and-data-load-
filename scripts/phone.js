06ad0f585ea2bc2dd585912b8155c5e90309ab45
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2024-11-11 00:53:01 +0600

    complete Api calling and Deta loading system

--- scripts/phone.js
+++ scripts/phone.js
@@ -1,8 +1,8 @@
-const loadData = async (sharchText) =>{ 
+const loadData = async (sharchText=12) =>{ 
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${sharchText}`);
     const data = await res.json();
     const allDatas = data.data;
-    console.log(allDatas);
+    // console.log(allDatas);
     desplayData(allDatas);
 
 }
@@ -37,9 +37,9 @@ else {
          </figure>
             <div class="card-body items-center text-center">
                   <h2 class="card-title">${element.phone_name}</h2>
-                   <p>${element.slug}</p>
+                   <p></p>
             <div class="card-actions">
-             <button class="btn btn-primary">Buy Now</button>
+             <button onclick="handelDetails('${element.slug}'); showDetailsModal.showModal()" class="btn btn-primary">Show Details</button>
          </div>
      </div>`
 
@@ -51,6 +51,34 @@ else {
 
 }
 
+const handelDetails = async(id)=>{
+    // console.log(id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const resJson = await res.json();
+    const showRes = resJson.data;
+    console.log(showRes);
+        
+    DesplayDetialsmodal(showRes);
+}
+
+const DesplayDetialsmodal = (phone)=>{
+    const phoneName = document.getElementById('phone_name');
+    phoneName.innerText=phone.name;
+    const phoneStorage = document.getElementById('storage');
+    phoneStorage.innerText = ("Storage : "+ phone.mainFeatures.storage)
+    const phoneDsSize  = document.getElementById('desplay_size');
+    phoneDsSize.innerText  = ("Display Size : "+ phone.mainFeatures.displaySize);
+    const phoneChipset = document.getElementById('chipset');
+    phoneChipset.innerText = ("Chipset : "+phone.mainFeatures.chipSet);
+    const phoneMemory  = document.getElementById('memory');
+    phoneMemory.innerText = ("Memory : "+phone.mainFeatures.memory)
+    const phoneRlsDate = document.getElementById('realise_date');
+    phoneRlsDate.innerText=("Release Date : "+ phone.releaseDate)
+    const phoneGps     = document.getElementById('gps');
+    phoneGps.innerText = ("Gps : "+ phone.others.GPS);
+
+    showDetailsModal.showModal()
+}
 // sharch button function
 
 const searchHandle = () => {
@@ -76,4 +104,4 @@ const loadingSpinner = (isLoading) => {
 }
 
 
-// loadData();
+loadData();

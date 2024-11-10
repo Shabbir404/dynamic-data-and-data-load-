c09c7f7f17a9ee8e20cb16844947ea55aea4c708
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2024-11-10 14:38:42 +0600

    latest version

--- scripts/phone.js
+++ scripts/phone.js
@@ -2,7 +2,7 @@ const loadData = async (sharchText) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${sharchText}`);
     const data = await res.json();
     const allDatas = data.data;
-    // console.log(allDatas);
+    console.log(allDatas);
     desplayData(allDatas);
 
 }
@@ -13,6 +13,17 @@ const desplayData = phones => {
    const phoneContainer = document.getElementById('phone-display-contrainer')
    //clearing phoneContainer
    phoneContainer.textContent = '';
+// setting <show all> button if the length of the phone is more then 12 
+const showAllBtnFind = document.getElementById('show_all_btn');
+if (phones.length > 12) {
+    showAllBtnFind.classList.remove('hidden');
+}
+else {
+    showAllBtnFind.classList.add('hidden');
+}
+
+   phones = phones.slice(0, 12);
+
    phones.forEach(element => {
     //  console.log(element);
      // step 1. creat a div
@@ -36,18 +47,32 @@ const desplayData = phones => {
        phoneContainer.appendChild(creatDiv);
     
    });
-
+   loadingSpinner(false);
 
 }
 
 // sharch button function
 
 const searchHandle = () => {
+    loadingSpinner(true);
    const sharchBoxfinder = document.getElementById('search-box');
    const sharchBoxText = sharchBoxfinder.value;
-   console.log(sharchBoxText)
-   loadData(sharchBoxText)
+   console.log(sharchBoxText);
+   loadData(sharchBoxText);
+ 
    
+}
+
+const loadingSpinner = (isLoading) => {
+ const loadingSpinnerFinder = document.getElementById('loadingDIv');
+ if (isLoading) {
+    loadingSpinnerFinder.classList.remove('hidden')
+     }
+     else{
+        loadingSpinnerFinder.classList.add('hidden');
+     }
+
+
 }
 
 
